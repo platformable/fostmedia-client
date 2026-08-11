f423199eb1f86b5fbb5a9daacfeae19fd53feed8
Alexei Garban <garban.valdeon@gmail.com>
2026-08-11 11:39:39 +0200

    fix: comment out redirect and matcher in proxy function

--- proxy.ts
+++ proxy.ts
@@ -2,9 +2,9 @@ import { NextResponse, NextRequest } from "next/server"
 
 export function proxy(request: NextRequest) {
   // Redirect to login page if not authenticated
-  return NextResponse.redirect(new URL("/tech-pulse", request.url))
+  // return NextResponse.redirect(new URL("/tech-pulse", request.url))
 }
 
 export const config = {
-  matcher: "/",
+  // matcher: "/",
 }
