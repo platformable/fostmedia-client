fcf783beffc54f1a652b1ec6fd37141daa954191
Alexei Garban <garban.valdeon@gmail.com>
2026-08-12 18:35:33 +0200

    feat: implement BlogPosts and IndustryPosts components for dynamic blog content rendering

---
+++ app/blog/[slug]/BlogPosts.tsx
@@ -0,0 +1,124 @@
+"use client"
+
+import React from "react"
+import BackToBlogBtn from "@/app/components/BackToBlogBtn"
+import BackToBlogFooter from "@/app/components/BackToBlogFooter"
+import LatestArticles from "@/app/components/LatestArticles"
+import Tag from "@/app/components/Tag"
+import getPostsBySlug from "@/utils/getPosts"
+import { useQuery } from "@tanstack/react-query"
+import Image from "next/image"
+import Link from "next/link"
+import calculateReadTime from "@/utils/calculateReadTime"
+import ShareWidget from "@/app/components/Share"
+import SponsorCarousel from "@/app/components/LogoCarousel"
+
+interface Props {
+  params: Promise<{
+    slug: string
+  }>
+}
+
+export default function BlogPosts({ params }: Props) {
+  const { slug } = React.use(params)
+
+  const {
+    data: posts,
+    isPending,
+    isError,
+  } = useQuery({
+    queryKey: ["postsBySlug", slug],
+    queryFn: () => getPostsBySlug(),
+  })
+
+  if (isPending) {
+    return <div>Loading...</div>
+  }
+
+  if (isError || !posts) {
+    return <div className="text-white">Failed to load posts.</div>
+  }
+
+  if (posts.length === 0) {
+    return <div className="text-white">No posts found.</div>
+  }
+
+  const post = posts[0]
+
+  return (
+    <section>
+      <div className="mx-auto max-w-screen-xl px-4 pt-10">
+        <div className="grid grid-cols-1 md:grid-cols-[8fr_4fr]">
+          <div>
+            <Tag text={posts[0]?.categories?.[0]?.Title || ""} section="blog" />
+            <h1 className="text-white text-base leading-10 my-5">
+              {posts[0]?.Title ||
+                "FOST launches a New Era for Conference Intelligence and Knowledge Discovery"}
+            </h1>
+            <span className="text-[#BCBCBC] text-sm">
+              {calculateReadTime(posts[0]?.Content || "")}
+            </span>
+          </div>
+          <div className="flex items-center justify-end gap-2 ">
+            <div className="flex gap-x-2 items-center">
+              <img
+                src="/avatar.jpg"
+                alt=""
+                className="w-10 h-10 rounded-full border border-[#40D2FF]"
+              />
+              <div className="">
+                <span className=" text-[#BCBCBC]">Mark Boyd </span> <br />
+                <Link href="#" className="text-[#40D2FF]">
+                  View profile →
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="relative h-64 w-full overflow-hidden rounded-2xl md:h-80 mt-10 mb-3">
+          <Image
+            src={
+              posts[0]?.Featured_Image?.url ||
+              "https://dummyimage.com/1920x1080/fff"
+            }
+            alt={slug}
+            fill
+            className="object-cover"
+            sizes="(max-width: 768px) 100vw, 50vw"
+          />
+        </div>
+
+        <section className="grid grid-cols-1 md:grid-cols-[8fr_4fr] gap-8 mt-5 ">
+          <div className="text-[#D6DAE0] border-r border-[#30323B] px-4">
+            <BackToBlogBtn section="blog" />
+
+            <div
+              dangerouslySetInnerHTML={{ __html: posts[0]?.Content || "" }}
+              id="post-content"
+            />
+
+            <BackToBlogFooter section="blog" />
+          </div>
+          <aside>
+            <div className="flex items-center gap-2 mb-5">
+              {/* <img src="/file.svg" width={20} height={20} alt="File Icon" />
+            <h6 className="main-color uppercase">On this page</h6> */}
+              {/*     <ShareModal /> */}
+            </div>
+            {/* <LatestArticles
+              description={false}
+              textSize={"LARGE"}
+              text="More from this creator"
+              section="blog"
+            /> */}
+            <ShareWidget section="blog" />
+            {/* <ShareModal /> */}
+          </aside>
+        </section>
+      </div>
+      <section>
+        <SponsorCarousel section="blog" />
+      </section>
+    </section>
+  )
+}
--- app/industry/[slug]/page.tsx
+++ app/industry/[slug]/page.tsx
@@ -1,131 +1,34 @@
-"use client"
-import React, { useEffect } from "react"
-import BackToBlogBtn from "@/app/components/BackToBlogBtn"
-import BackToBlogFooter from "@/app/components/BackToBlogFooter"
-import LatestArticles from "@/app/components/LatestArticles"
-import Tag from "@/app/components/Tag"
-import getPosts, { getPostsBySlug } from "@/utils/getPosts"
-import { useQuery } from "@tanstack/react-query"
-import Image from "next/image"
-import Link from "next/link"
-import calculateReadTime from "@/utils/calculateReadTime"
-import ShareModal from "@/app/components/ShareModal"
-import EventCarousel from "@/app/components/Blogcarousel"
-import SponsorCarousel from "@/app/components/LogoCarousel"
-import ShareWidget from "@/app/components/Share"
+import type { Metadata } from "next"
+import { getPostsBySlug } from "@/utils/getPosts"
 import createShareMetadata from "@/lib/metadata"
-// import ShareMetadata from "@/lib/metadata";
+import BlogPosts from "@/app/industry/[slug]/IndustryPosts"
 
-export default function page({
+interface PageProps {
+  params: Promise<{
+    slug: string
+  }>
+}
+
+export async function generateMetadata({
   params,
-}: {
-  params: Promise<{ slug: string }>
-}) {
-  const { slug } = React.use(params)
-  const {
-    data: posts,
-    isPending,
-    isError,
-  } = useQuery({
-    queryKey: ["postsBySlug", slug],
-    queryFn: () => getPostsBySlug(slug),
-  })
+}: PageProps): Promise<Metadata> {
+  const { slug } = await params
 
-  /*   console.log(posts, "posts") */
-  if (isPending) {
-    return <div>Loading...</div>
-  }
-  if (isError || !posts) {
-    return <div className="text-white">Failed to load posts.</div>
-  }
-  if (posts.length === 0) {
-    return <div className="text-white">No posts found.</div>
+  const posts = await getPostsBySlug(slug)
+  const post = posts?.[0]
+
+  if (!post) {
+    return {}
   }
 
-  const metadata = createShareMetadata({
-    title: posts[0]?.Title,
-    description: posts[0]?.excerpt || "",
+  return createShareMetadata({
+    title: post.Title,
+    description: post.excerpt || "",
     url: `/industry/${slug}`,
-    image: posts[0]?.Featured_Image?.url,
+    image: post.Featured_Image?.url,
   })
+}
 
-  return (
-    <section>
-      <div className="mx-auto max-w-screen-xl px-4 pt-10">
-        <div className="grid grid-cols-1 md:grid-cols-[8fr_4fr]">
-          <div>
-            <Tag
-              text={posts[0]?.categories?.[0]?.Title || ""}
-              section="industry"
-            />
-            <h1 className="text-white text-base leading-10 my-5">
-              {posts[0]?.Title ||
-                "FOST launches a New Era for Conference Intelligence and Knowledge Discovery"}
-            </h1>
-            <span className="text-[#BCBCBC] text-sm">
-              {calculateReadTime(posts[0]?.Content || "")}
-            </span>
-          </div>
-          <div className="flex items-center justify-end gap-2 ">
-            <div className="flex gap-x-2 items-center">
-              <img
-                src="/avatar.jpg"
-                alt=""
-                className="w-10 h-10 rounded-full border border-[#40D2FF]"
-              />
-              <div className="">
-                <span className=" text-[#BCBCBC]">Mark Boyd </span> <br />
-                <Link href="#" className="text-[#40D2FF]">
-                  View profile →
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="relative h-64 w-full overflow-hidden rounded-2xl md:h-80 mt-10 mb-3">
-          <Image
-            src={
-              posts[0]?.Featured_Image?.url ||
-              "https://dummyimage.com/1920x1080/fff"
-            }
-            alt={slug}
-            fill
-            className="object-cover"
-            sizes="(max-width: 768px) 100vw, 50vw"
-          />
-        </div>
-
-        <section className="grid grid-cols-1 md:grid-cols-[8fr_4fr] gap-8 mt-5 ">
-          <div className="text-[#D6DAE0] border-r border-[#30323B] px-4">
-            <BackToBlogBtn section="industry" />
-
-            <div
-              dangerouslySetInnerHTML={{ __html: posts[0]?.Content || "" }}
-              id="post-content"
-            />
-
-            <BackToBlogFooter section="industry" />
-          </div>
-          <aside>
-            <div className="flex items-center gap-2 mb-5">
-              {/* <img src="/file.svg" width={20} height={20} alt="File Icon" />
-            <h6 className="main-color uppercase">On this page</h6> */}
-              {/*     <ShareModal /> */}
-            </div>
-            {/* <LatestArticles
-              description={false}
-              textSize={"LARGE"}
-              text="More from this creator"
-              section="industry"
-            /> */}
-            <ShareWidget section="industry" />
-            {/* <ShareModal /> */}
-          </aside>
-        </section>
-      </div>
-      <section>
-        <SponsorCarousel section="industry" />
-      </section>
-    </section>
-  )
+export default function Page({ params }: PageProps) {
+  return <BlogPosts params={params} />
 }
